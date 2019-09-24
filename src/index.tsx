9f748a5e5be4d8bfa79eadf1c036bec537964b84
Carl Bentham <carlbentham@gmail.com>
2019-09-24 18:57:05 +0200

    FC with useState + typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const MyComp: React.FC = () => (
-  <div>Hello</div>
-)
+const Root = document.getElementById('root');
 
-ReactDOM.render(<MyComp />, document.getElementById('root'));
+type HelloProps = {
+  name: string;
+};
+
+const Hello: React.FC = () => {
+  let [clicked, setClicked] = React.useState(false);
+
+  const toggleClicked = () => {
+    setClicked(!clicked);
+  };
+
+  return (
+    <div className={clicked ? 'clicked' : ''} onClick={toggleClicked}>
+      Hello! I am {clicked ? 'clicked' : 'not clicked'}
+    </div>
+  );
+};
+
+ReactDOM.render(<Hello />, Root);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
