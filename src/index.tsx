a7f13c1355ce19ca6f335d3e79b8b17d8e5683af
Carl Bentham <carlbentham@gmail.com>
2019-09-25 17:29:52 +0200

    made 'selectedStation' a state var of parent StationList so can be shared with all Station children. Station Click => Update State => Potential rerender of all stations with updated props => Newly clicked station opens/closes, old open ones close

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 // import React, { Component, useState } from 'react';
-import React, { Component, ReactElement, useState } from 'react';
+import React, { ReactElement, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
 //import App from './App';
@@ -8,72 +8,81 @@ import * as serviceWorker from './serviceWorker';
 const Root = document.getElementById('root');
 
 const Header: React.FC = () => {
-  return (
-    <div className='header'>Header</div>
-  )
-}
+  return <div className="header">Header</div>;
+};
 
 const Footer: React.FC = () => {
-  return (
-    <div className="footer">Footer</div>
-  )
-}
+  return <div className="footer">Footer</div>;
+};
 
 const StationExpansion: React.FC = () => {
-  return (
-    <div className='expansion'>Expanded</div>
-  )
-}
+  return <div className="expansion">Expanded</div>;
+};
 
-const Station: React.FC = () => {
+type StationProps = {
+  id: number;
+  currentStation: number | null;
+  updateSelected: React.Dispatch<React.SetStateAction<null | number>>;
+};
+
+const Station: React.FC<StationProps> = props => {
   // hooks and 'set' func must be inside their comps (obviously)
-  let [stationSelected, setStationSelected] = useState(false);
+  let expand = () => {
+    return props.currentStation === props.id;
+  };
 
-  const toggleStationSelected = () => {
-    setStationSelected(!stationSelected);
-   }
+  const useToggleStationSelected = () => {
+    if (props.currentStation === props.id) {
+      props.updateSelected(null);
+    } else {
+      props.updateSelected(props.id);
+    }
+  };
 
-  // jsx can appear inside js code wrapped in { }. Further js
-  //   inside this requires addional braces, i.e. outer {} reset
-  //   inside jsx
   return (
-    <div className='station' onClick={ toggleStationSelected }>
-      { stationSelected ? <StationExpansion /> : null }
-      <p className='station-name'>Cool Radio</p>
-      <p className='station-freq'>100.1</p>
+    <div className="station" onClick={useToggleStationSelected}>
+      {expand() ? <StationExpansion /> : null}
+      <p className="station-name">Cool Radio</p>
+      <p className="station-freq">100.1</p>
     </div>
-  )
-}
-
-let stations: ReactElement[] = [];
-
-// build list dyanmically
-for(let i = 0; i < 5; i++) {
-  stations.push(<Station />);
-}
+  );
+};
 
 const StationList: React.FC = () => {
+  let [selectedStation, setSelectedStation] = useState();
+  let stations: ReactElement[] = [];
+
+  // build list dyanmically
+  for (let i = 0; i < 5; i++) {
+    stations.push(
+      <Station
+        key={i}
+        id={i}
+        currentStation={selectedStation}
+        updateSelected={setSelectedStation}
+      />
+    );
+  }
+
   return (
-    <div className='station-list'>List
-    { stations }
-
+    <div className="station-list">
+      List
+      {stations}
     </div>
-  )
-}
-
-
+  );
+};
 
 const App: React.FC = () => (
-    <>
-      <Header />
-      <StationList />
-      <Footer />
-    </>
-  )
+  <>
+    <Header />
+    <StationList />
+    <Footer />
+  </>
+);
 
 ReactDOM.render(<App />, Root);
 
 // If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
+// unregister() to registner() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
