4dfb352769827d706518155e8c2d61ddfa116422
Carl Bentham <carlbentham@gmail.com>
2019-09-25 13:26:23 +0200

    create basic radio station layout with 'onclick' expanding cards

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,77 @@
-import React, { Component, useState } from 'react';
+// import React, { Component, useState } from 'react';
+import React, { Component, ReactElement, useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.scss';
 //import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const Root = document.getElementById('root');
 
-type HelloProps = {
-  name: string;
-};
-
-const Hello: React.FC = () => {
-  let [clicked, setClicked] = React.useState(false);
-
-  const toggleClicked = () => {
-    setClicked(!clicked);
-  };
-
+const Header: React.FC = () => {
   return (
-    <div className={clicked ? 'clicked' : ''} onClick={toggleClicked}>
-      Hello! I am {clicked ? 'clicked' : 'not clicked'}
-    </div>
-  );
-};
+    <div className='header'>Header</div>
+  )
+}
 
-ReactDOM.render(<Hello />, Root);
+const Footer: React.FC = () => {
+  return (
+    <div className="footer">Footer</div>
+  )
+}
+
+const StationExpansion: React.FC = () => {
+  return (
+    <div className='expansion'>Expanded</div>
+  )
+}
+
+const Station: React.FC = () => {
+  // hooks and 'set' func must be inside their comps (obviously)
+  let [stationSelected, setStationSelected] = useState(false);
+
+  const toggleStationSelected = () => {
+    setStationSelected(!stationSelected);
+   }
+
+  // jsx can appear inside js code wrapped in { }. Further js
+  //   inside this requires addional braces, i.e. outer {} reset
+  //   inside jsx
+  return (
+    <div className='station' onClick={ toggleStationSelected }>
+      { stationSelected ? <StationExpansion /> : null }
+      <p className='station-name'>Cool Radio</p>
+      <p className='station-freq'>100.1</p>
+    </div>
+  )
+}
+
+let stations: ReactElement[] = [];
+
+// build list dyanmically
+for(let i = 0; i < 5; i++) {
+  stations.push(<Station />);
+}
+
+const StationList: React.FC = () => {
+  return (
+    <div className='station-list'>List
+    { stations }
+
+    </div>
+  )
+}
+
+
+
+const App: React.FC = () => (
+    <>
+      <Header />
+      <StationList />
+      <Footer />
+    </>
+  )
+
+ReactDOM.render(<App />, Root);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
