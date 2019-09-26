1592b51be9175325b96623e79208a8595322eae4
Carl Bentham <carlbentham@gmail.com>
2019-09-26 19:30:09 +0200

    catch api error

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -4,32 +4,67 @@ import StationList from './station-list';
 import Header from './header';
 import Footer from './footer';
 
+const TEST_ERR = false; // true = simulate api fetch error
+const URL = `https://teclead.de/recruiting/${TEST_ERR ? '' : 'radios'}`;
+
 const App: React.FC = () => {
   const [stations, setStations] = useState<any[]>([]);
-  let [selectedStation, setSelectedStation] = useState();
+  const [selectedStation, setSelectedStation] = useState();
+  const [errorMessage, setErrorMessage] = useState<null | string>(null);
 
   useMemo(async () => {
-    const body = await fetch('https://teclead.de/recruiting/radios');
-    const stations = (await body.json()).radios;
-    stations.forEach( (station: Station, idx: number) => station.id = idx + 1);
-    setStations(stations);
+    try {
+      const body = await fetch(URL);
+      const stations = (await body.json()).radios;
+      stations.forEach( (station: Station, idx: number) => station.id = idx + 1);
+      setStations(stations);
+    }
+    catch {
+      console.log('ERROR');
+      setErrorMessage("Unable to find list of stations.");
+    }
   }, []);
 
   const selectedStationName = () => {
     return selectedStation ? stations[selectedStation - 1].name: null;
   };
 
-  return (
-    <div className="app-container">
-      <Header />
-      <StationList
-        stations={stations}
-        selectedStation={selectedStation}
-        updateSelected={setSelectedStation}
-       />
-      <Footer stationName={selectedStationName()}/>
+  type ErrorProps = {
+    message: string;
+  }
+
+  const Error: React.FC<ErrorProps> = (props) => (
+    <div className="error-message">
+      <p>{ props.message }</p>
+      <i className="far fa-frown"></i>
     </div>
-   );
+  )
+
+  console.log(errorMessage);
+
+  if (errorMessage) {
+    return  (
+      <div className="app-container">
+      <Header />
+      <Error message={errorMessage} />
+      { console.log('HERE') }
+      <Footer stationName={selectedStationName()}/>
+      </div>
+    )
+  }
+  else {
+    return (
+      <div className="app-container">
+        <Header />
+        <StationList
+          stations={stations}
+          selectedStation={selectedStation}
+          updateSelected={setSelectedStation}
+        />
+        <Footer stationName={selectedStationName()}/>
+      </div>
+    );
+  }
 };
 
 export default App;
